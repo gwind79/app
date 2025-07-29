ce1e40d386724829403857728fa831db292870c5
gwind79 <gwind79@users.noreply.github.com>
2025-07-29 23:32:15 +0200

    Update app.py

--- app.py
+++ app.py
@@ -1,14 +1,16 @@
 import subprocess
 import time
+import os
 from flask import Flask, request, jsonify
-from flask_cors import CORS  # <--- IMPORTA QUESTO
+from flask_cors import CORS
 
 app = Flask(__name__)
+CORS(app)  # Abilita CORS
 
 @app.route('/start', methods=['GET'])
 def start_calculator():
     if os.environ.get("RUNNING_ON_RENDER") != "1":
-      subprocess.Popen(['python', 'script_locale.py'], shell=True)
+        subprocess.Popen(['python', 'script_locale.py'], shell=True)
     time.sleep(1)
     return "Roboclick avviato"
 
@@ -16,10 +18,9 @@ def start_calculator():
 def press_key():
     key = request.json.get('key')
     if key:
-        return f"Premuto: {key}"
+        return jsonify({"status": "ok", "key": key})
     return "Chiave non valida", 400
 
 if __name__ == '__main__':
-    app.run(host='https://app-1-anm0.onrender.com/', port=5000)
-    #app.run(host='80.21.248.146', port=5000)
-    
+    port = int(os.environ.get('PORT', 5000))  # Porta dinamica su Render
+    app.run(host='0.0.0.0', port=port)
