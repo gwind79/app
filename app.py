68e9e19f06b53b77b3c1746400312bb57291e1f2
gwind79 <gwind79@users.noreply.github.com>
2025-07-29 23:04:38 +0200

    Update app.py

--- app.py
+++ app.py
@@ -1,25 +1,28 @@
-from flask import Flask, request, jsonify
-from flask_cors import CORS  # <--- IMPORTA QUESTO
+from flask import Flask, request
+import subprocess
+import pyautogui
+import time
 
 app = Flask(__name__)
-CORS(app)  # <--- ABILITA CORS PER TUTTE LE ROTTE
 
-AUTH_TOKEN = "mio_token_sicuro"
-
-@app.route('/')
-def home():
-    return "Server Flask attivo su Render!"
+@app.route('/start', methods=['GET'])
+def start_calculator():
+    #subprocess.Popen('calc.exe')
+    path = os.getcwd()
+    path1 = os.path.join('python', "righello_adv.py") 
+    subprocess.Popen(path1)
+    time.sleep(1)
+    return "Roboclick avviato"
 
 @app.route('/press', methods=['POST'])
 def press_key():
-    token = request.headers.get('Authorization')
-    if token != AUTH_TOKEN:
-        return "Accesso negato", 403
     key = request.json.get('key')
-    print(f"Comando ricevuto: {key}")
-    return jsonify({"status": "ok", "key": key})
+    if key:
+        pyautogui.press(key)
+        return f"Premuto: {key}"
+    return "Chiave non valida", 400
 
 if __name__ == '__main__':
-    import os
-    port = int(os.environ.get('PORT', 5000))
-    app.run(host='0.0.0.0', port=port)
+    app.run(host='https://app-1-anm0.onrender.com/', port=5000)
+    #app.run(host='80.21.248.146', port=5000)
+    
