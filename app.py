ed7c6ec0a8d5fb0083c4537aea2ef178a731da3c
gwind79 <gwind79@users.noreply.github.com>
2025-07-29 23:40:40 +0200

    Update app.py

--- app.py
+++ app.py
@@ -4,6 +4,9 @@ import os
 from flask import Flask, request, jsonify
 from flask_cors import CORS
 
+@app.route('/')
+def home():
+    return "Server Flask attivo su Render!"
 app = Flask(__name__)
 CORS(app)  # Abilita CORS
 
